788a18010b74c80b14ed93c31d37b814111ffb50
chengzhi610 <zhw13470097602@petalmail.com>
2026-02-26 09:31:34 +0800

    Add file write to verify JNI call

--- FCLauncher/src/main/cpp/adrenotools_jni.cpp
+++ FCLauncher/src/main/cpp/adrenotools_jni.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <adrenotools/driver.h>
 #include <android/log.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define LOG_TAG "adrenotools_jni"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -13,6 +15,13 @@ JNIEXPORT void JNICALL
 Java_com_tungsten_fclauncher_FCLauncher_initAdrenotools(JNIEnv *env, jclass clazz,
                                                          jstring nativeLibDir,
                                                          jstring sdcardDriverDir) {
+    // 写入文件以验证是否被调用
+    int fd = open("/data/data/com.tungsten.fcl.debug/adrenotools_test.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd != -1) {
+        write(fd, "initAdrenotools called\n", 24);
+        close(fd);
+    }
+
     const char *native_dir = env->GetStringUTFChars(nativeLibDir, nullptr);
     const char *sdcard_dir = env->GetStringUTFChars(sdcardDriverDir, nullptr);
 
